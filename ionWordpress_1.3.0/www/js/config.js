a4330742b4df54ec9cf9ebfb1241afd92188429a
elyas_yavari <elyasyavari@gmail.com>
2016-03-02 08:19:49 +0330

    core

--- ionWordpress_1.3.0/www/js/config.js
+++ ionWordpress_1.3.0/www/js/config.js
@@ -1,5 +1,5 @@
 angular.module('your_app_name.config', [])
-.constant('WORDPRESS_API_URL', 'http://www.radioogle.ir/api/')
+.constant('WORDPRESS_API_URL', 'http://wordpress.startapplabs.com/blog/api/')
 .constant('WORDPRESS_PUSH_URL', 'http://wordpress.startapplabs.com/blog/push/')
 .constant('GCM_SENDER_ID', '574597432927')
 
